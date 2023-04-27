484007f6e86d94ff301a40b6ba1c49161d6deb6d
Nikita Karpuk <bitxon.dev@gmail.com>
2023-04-27 19:55:35 +0200

    Spring 3.1.0

--- spring-app/src/test/java/bitxon/spring/test/AbstractSpringTest.java
+++ spring-app/src/test/java/bitxon/spring/test/AbstractSpringTest.java
@@ -1,57 +1,32 @@
 package bitxon.spring.test;
 
 import bitxon.spring.SpringAppApplication;
+import bitxon.spring.test.ext.TestcontainersConfig;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.BindMode;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
-import org.testcontainers.lifecycle.Startables;
-import org.testcontainers.utility.DockerImageName;
 
 @ActiveProfiles("test")
+@Import(TestcontainersConfig.class)
 @SpringBootTest(classes = SpringAppApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 abstract class AbstractSpringTest {
-
-    static PostgreSQLContainer DB = (PostgreSQLContainer) new PostgreSQLContainer(DockerImageName.parse("postgres").withTag("14.4"))
-        .withDatabaseName("testdb")
-        .withUsername("postgres")
-        .withPassword("postgres")
-        .withInitScript("sql/db-test-data.sql");
-    static GenericContainer WIREMOCK = new GenericContainer("wiremock/wiremock:2.35.0")
-        .withExposedPorts(8080)
-        .withClasspathResourceMapping("stubs", "/home/wiremock", BindMode.READ_ONLY)
-        .waitingFor(Wait
-            .forHttp("/__admin/mappings")
-            .withMethod("GET")
-            .forStatusCode(200));
-
-    static {
-        Startables.deepStart(DB, WIREMOCK).join();
-    }
-
-    @DynamicPropertySource
-    static void dynamicProperty(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", DB::getJdbcUrl);
-        registry.add("spring.datasource.username", DB::getUsername);
-        registry.add("spring.datasource.password", DB::getPassword);
-        registry.add("wiremock.server.port", () -> WIREMOCK.getMappedPort(8080));
-    }
 
     @LocalServerPort
     private Integer port;
 
+    @Autowired
+    private GenericContainer wiremockContainer;
+
     @BeforeEach
     public void setUp() {
         RestAssured.port = port;
-        WireMock.configureFor(WIREMOCK.getMappedPort(8080));
+        WireMock.configureFor(wiremockContainer.getHost(), wiremockContainer.getMappedPort(8080));
     }
 
 }
---
+++ spring-app/src/test/java/bitxon/spring/test/ext/TestcontainersConfig.java
@@ -0,0 +1,44 @@
+package bitxon.spring.test.ext;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestcontainersConfig {
+
+    @Bean
+    @ServiceConnection
+    public PostgreSQLContainer postgreSQLContainer() {
+        return (PostgreSQLContainer) new PostgreSQLContainer("postgres:14.4")
+                .withDatabaseName("testdb")
+                .withUsername("postgres")
+                .withPassword("postgres")
+                .withInitScript("sql/db-test-data.sql");
+    }
+
+    @Bean
+    public GenericContainer wiremockContainer(DynamicPropertyRegistry registry) {
+        var container = new GenericContainer("wiremock/wiremock:2.35.0")
+                .withExposedPorts(8080)
+                .withClasspathResourceMapping("stubs", "/home/wiremock", BindMode.READ_ONLY)
+                .waitingFor(Wait
+                        .forHttp("/__admin/mappings")
+                        .withMethod("GET")
+                        .forStatusCode(200));
+
+
+        registry.add("http.exchange-client.url", () -> httpUrl(container.getHost(), container.getMappedPort(8080)));
+
+        return container;
+    }
+
+    private static String httpUrl(String host, Integer port) {
+        return String.format("http://%s:%d", host, port);
+    }
+}
