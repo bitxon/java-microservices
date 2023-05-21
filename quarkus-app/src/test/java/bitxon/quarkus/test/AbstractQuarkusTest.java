11340bddb64db2913709cf21621c4cd4df29067f
Nikita Karpuk <bitxon.dev@gmail.com>
2023-05-21 15:45:31 +0200

    Quarkus: Inject test resource from manager

--- quarkus-app/src/test/java/bitxon/quarkus/test/AbstractQuarkusTest.java
+++ quarkus-app/src/test/java/bitxon/quarkus/test/AbstractQuarkusTest.java
@@ -2,10 +2,23 @@ package bitxon.quarkus.test;
 
 import bitxon.quarkus.test.ext.PostgresTestResourceLifecycleManager;
 import bitxon.quarkus.test.ext.WiremockTestResourceLifecycleManager;
+import com.github.tomakehurst.wiremock.client.WireMock;
 import io.quarkus.test.common.QuarkusTestResource;
+import org.junit.jupiter.api.BeforeEach;
+import org.testcontainers.containers.GenericContainer;
 
 @QuarkusTestResource(PostgresTestResourceLifecycleManager.class)
 @QuarkusTestResource(WiremockTestResourceLifecycleManager.class)
 public abstract class AbstractQuarkusTest {
+
+
+    // Will be injected by {WiremockTestResourceLifecycleManager#inject()}
+    @WiremockTestResourceLifecycleManager.Inject
+    GenericContainer wiremockContainer;
+
+    @BeforeEach
+    public void beforeEach() {
+        WireMock.configureFor(wiremockContainer.getMappedPort(8080));
+    }
 
 }
--- quarkus-app/src/test/java/bitxon/quarkus/test/ext/WiremockTestResourceLifecycleManager.java
+++ quarkus-app/src/test/java/bitxon/quarkus/test/ext/WiremockTestResourceLifecycleManager.java
@@ -1,21 +1,24 @@
 package bitxon.quarkus.test.ext;
 
-import com.github.tomakehurst.wiremock.client.WireMock;
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.MountableFile;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.util.Map;
 
 public class WiremockTestResourceLifecycleManager implements QuarkusTestResourceLifecycleManager {
 
-    private GenericContainer wiremock;
+    private GenericContainer wiremockContainer;
 
 
     @Override
     public void init(Map<String, String> initArgs) {
-        wiremock = new GenericContainer("wiremock/wiremock:2.35.0")
+        wiremockContainer = new GenericContainer("wiremock/wiremock:2.35.0")
             .withExposedPorts(8080)
             .withCopyFileToContainer(MountableFile.forClasspathResource("stubs"), "/home/wiremock")
             .waitingFor(Wait
@@ -26,18 +29,31 @@ public class WiremockTestResourceLifecycleManager implements QuarkusTestResource
 
     @Override
     public Map<String, String> start() {
-        wiremock.start();
-        WireMock.configureFor(wiremock.getMappedPort(8080));
+        wiremockContainer.start();
 
         return Map.of(
-            "quarkus.rest-client.exchange-client.url", String.format("http://%s:%d", wiremock.getHost(), wiremock.getMappedPort(8080))
+            "quarkus.rest-client.exchange-client.url", String.format("http://%s:%d", wiremockContainer.getHost(), wiremockContainer.getMappedPort(8080))
         );
     }
 
     @Override
     public void stop() {
-        if (wiremock != null) {
-            wiremock.stop();
+        if (wiremockContainer != null) {
+            wiremockContainer.stop();
         }
+    }
+
+    @Override
+    public void inject(TestInjector testInjector) {
+        testInjector.injectIntoFields(
+            wiremockContainer,
+            field -> field.isAnnotationPresent(Inject.class)
+        );
+    }
+
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.FIELD)
+    public @interface Inject {
     }
 }
