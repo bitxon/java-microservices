6de3e0030984b0ba57b7232c226a5ae6c8b70c49
Nikita Karpuk <bitxon.dev@gmail.com>
2023-09-06 19:31:18 +0200

    All: Align clients timeout

--- spring-app/src/main/java/bitxon/spring/client/ExchangeClientConfig.java
+++ spring-app/src/main/java/bitxon/spring/client/ExchangeClientConfig.java
@@ -7,14 +7,19 @@ import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
+import java.time.Duration;
+
 @Configuration
 public class ExchangeClientConfig {
 
     @Bean
-    public ExchangeClient exchangeClient(@Value("${http.exchange-client.url}") String url) {
-        var webClient = WebClient.create(url);
-        var factory = HttpServiceProxyFactory.builder(WebClientAdapter.forClient(webClient)).build();
-        var serviceClient = factory.createClient(ExchangeClient.class);
-        return serviceClient;
+    public ExchangeClient exchangeClient(@Value("${http.exchange-client.url}") String url,
+                                         @Value("${http.exchange-client.block-timeout}") Duration blockTimeout,
+                                         WebClient.Builder webClientBuilder) {
+        var webClient = webClientBuilder.baseUrl(url).build();
+        var factory = HttpServiceProxyFactory.builder(WebClientAdapter.forClient(webClient))
+            .blockTimeout(blockTimeout)
+            .build();
+        return factory.createClient(ExchangeClient.class);
     }
 }
