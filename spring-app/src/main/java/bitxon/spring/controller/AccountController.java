c62758b9c66db481109224cb4c7f07313eeb7b34
Nikita Karpuk <bitxon.dev@gmail.com>
2023-09-06 21:00:06 +0200

    Spring: Manual Transaction Controll

--- spring-app/src/main/java/bitxon/spring/controller/AccountController.java
+++ spring-app/src/main/java/bitxon/spring/controller/AccountController.java
@@ -2,6 +2,7 @@ package bitxon.spring.controller;
 
 import static bitxon.common.api.constant.Constants.DIRTY_TRICK_HEADER;
 import static bitxon.common.api.constant.Constants.DirtyTrick.FAIL_TRANSFER;
+import static org.springframework.transaction.annotation.Propagation.NEVER;
 
 import bitxon.common.api.model.Account;
 import bitxon.common.api.model.MoneyTransfer;
@@ -14,6 +15,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,6 +33,8 @@ import java.util.stream.Collectors;
 @RequestMapping("/accounts")
 @RequiredArgsConstructor
 public class AccountController {
+
+    private final TransactionTemplate transactionTemplate;
 
     private final AccountDao dao;
     private final AccountMapper mapper;
@@ -64,27 +68,42 @@ public class AccountController {
 
     @PostMapping("/transfers")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @Transactional
+    @Transactional(propagation = NEVER)
     public void transfer(@Valid @RequestBody MoneyTransfer transfer,
                          @RequestHeader(value = DIRTY_TRICK_HEADER, required = false) String dirtyTrick) {
 
-        var sender = dao.findById(transfer.senderId())
-            .orElseThrow(() -> new ResourceNotFoundException("Sender not found"));
+        var loadResult = transactionTemplate.execute(transactionStatus -> {
+            return new LoadResult(
+                dao.findById(transfer.senderId())
+                    .orElseThrow(() -> new ResourceNotFoundException("Sender not found")),
+                dao.findById(transfer.recipientId())
+                    .orElseThrow(() -> new ResourceNotFoundException("Recipient not found"))
+            );
+        });
 
-        var recipient = dao.findById(transfer.recipientId())
-            .orElseThrow(() -> new ResourceNotFoundException("Recipient not found"));
+        var sender = loadResult.sender();
+        var recipient = loadResult.recipient();
 
         var exchangeRateValue = exchangeClient.getExchangeRate(sender.getCurrency())
             .rates().getOrDefault(recipient.getCurrency(), 1.0);
 
-        sender.setMoneyAmount(sender.getMoneyAmount() - transfer.moneyAmount());
-        dao.save(sender);
+        transactionTemplate.executeWithoutResult(transactionStatus -> {
+            sender.setMoneyAmount(sender.getMoneyAmount() - transfer.moneyAmount());
+            dao.save(sender);
 
-        if (FAIL_TRANSFER.equals(dirtyTrick)) {
-            throw new DirtyTrickException("Error during money transfer");
-        }
+            if (FAIL_TRANSFER.equals(dirtyTrick)) {
+                throw new DirtyTrickException("Error during money transfer");
+            }
 
-        recipient.setMoneyAmount(recipient.getMoneyAmount() + (int) (transfer.moneyAmount() * exchangeRateValue));
-        dao.save(recipient);
+            recipient.setMoneyAmount(recipient.getMoneyAmount() + (int) (transfer.moneyAmount() * exchangeRateValue));
+            dao.save(recipient);
+        });
+    }
+
+
+    public record LoadResult(
+        bitxon.spring.db.model.Account sender,
+        bitxon.spring.db.model.Account recipient
+    ) {
     }
 }
