d32b0002daa786ebc798c7f5acea7bd967f7388c
Nikita Karpuk <bitxon.dev@gmail.com>
2023-12-02 20:09:24 +0100

    Spring: 3.2.0

--- spring-app/src/main/java/bitxon/spring/client/ExchangeClientConfig.java
+++ spring-app/src/main/java/bitxon/spring/client/ExchangeClientConfig.java
@@ -3,8 +3,9 @@ package bitxon.spring.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.support.WebClientAdapter;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 import java.time.Duration;
@@ -14,12 +15,17 @@ public class ExchangeClientConfig {
 
     @Bean
     public ExchangeClient exchangeClient(@Value("${http.exchange-client.url}") String url,
-                                         @Value("${http.exchange-client.block-timeout}") Duration blockTimeout,
-                                         WebClient.Builder webClientBuilder) {
-        var webClient = webClientBuilder.baseUrl(url).build();
-        var factory = HttpServiceProxyFactory.builder(WebClientAdapter.forClient(webClient))
-            .blockTimeout(blockTimeout)
+                                         @Value("${http.exchange-client.read-timeout}") Duration readTimeout,
+                                         RestClient.Builder restClientBuilder) {
+        var requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setReadTimeout(readTimeout);
+
+        var restClient = restClientBuilder
+            .baseUrl(url)
+            .requestFactory(requestFactory)
             .build();
+
+        var factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
         return factory.createClient(ExchangeClient.class);
     }
 }
