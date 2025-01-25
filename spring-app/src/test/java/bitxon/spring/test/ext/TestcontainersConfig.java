b273d4b2975a1374edb7f2109c73d7ce5985d8d2
Nikita Karpuk <bitxon.dev@gmail.com>
2025-01-25 12:35:23 +0100

    Spring: 3.4.2

--- spring-app/src/test/java/bitxon/spring/test/ext/TestcontainersConfig.java
+++ spring-app/src/test/java/bitxon/spring/test/ext/TestcontainersConfig.java
@@ -3,6 +3,7 @@ package bitxon.spring.test.ext;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistrar;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -11,6 +12,13 @@ import org.testcontainers.utility.MountableFile;
 
 @TestConfiguration(proxyBeanMethods = false)
 public class TestcontainersConfig {
+
+    @Bean
+    public DynamicPropertyRegistrar propertiesOverride(GenericContainer wiremockContainer) {
+        return (registry) ->
+            registry.add("http.exchange-client.url", () -> httpUrl(wiremockContainer, 8080));
+
+    }
 
     @Bean
     @ServiceConnection
@@ -23,22 +31,21 @@ public class TestcontainersConfig {
     }
 
     @Bean
-    public GenericContainer wiremockContainer(DynamicPropertyRegistry registry) {
-        var container = new GenericContainer("wiremock/wiremock:3.0.0-1")
+    public GenericContainer wiremockContainer() {
+        return new GenericContainer("wiremock/wiremock:3.0.0-1")
             .withExposedPorts(8080)
             .withCopyFileToContainer(MountableFile.forClasspathResource("stubs"), "/home/wiremock")
             .waitingFor(Wait
                 .forHttp("/__admin/mappings")
                 .withMethod("GET")
                 .forStatusCode(200));
-
-
-        registry.add("http.exchange-client.url", () -> httpUrl(container.getHost(), container.getMappedPort(8080)));
-
-        return container;
     }
 
     private static String httpUrl(String host, Integer port) {
         return String.format("http://%s:%d", host, port);
+    }
+
+    private static String httpUrl(GenericContainer container, Integer originalPort) {
+        return httpUrl(container.getHost(), container.getMappedPort(originalPort));
     }
 }
